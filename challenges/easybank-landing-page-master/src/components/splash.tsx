a6534ac26268599a45a7454db472b6948ecba67f
Greg Schultz <gjs86@cornell.edu>
2020-03-29 13:42:09 -0400

    most details of image done, find way to get overflow correct

--- challenges/easybank-landing-page-master/src/components/splash.tsx
+++ challenges/easybank-landing-page-master/src/components/splash.tsx
@@ -17,7 +17,8 @@ const Splash = () => {
 const LeftSplash = () => {
   return (
     <section id="splash-left" className="w-7/12">
-      <h3 className="align-baseline"> Next generation digital banking</h3>
+      <h3> Next generation </h3>
+      <h3> digital banking</h3>
       <p>
         Take your financial life online. Your Easybank account will be a
         one-stop-shop for spending, saving, budgeting, investing, and much more.
