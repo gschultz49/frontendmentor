67b975ff743e25f9b5afe3016f1a32964a1ae83a
Greg Schultz <gjs86@cornell.edu>
2020-03-29 00:26:29 -0400

    left side of splash

--- challenges/easybank-landing-page-master/src/components/splash.tsx
+++ challenges/easybank-landing-page-master/src/components/splash.tsx
@@ -1,9 +1,23 @@
 import BodyContainer from "./body-container";
+import Button from "./button";
 
-const Splash = props => {
+const Splash = () => {
   return (
-    <section id="splash">
-      <h1> Next Generation Digital Banking</h1>
+    <section id="splash" className="flex">
+      <section id="splash-content" className="flex-1 align-middle py-32">
+        <h3 className="align-baseline"> Next generation digital banking</h3>
+        <p>
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <section>
+          <Button label={"Request Invite"} />
+        </section>
+      </section>
+      <section className="flex-1 align-middle">
+        <h1> photo meeee</h1>
+      </section>
     </section>
   );
 };
