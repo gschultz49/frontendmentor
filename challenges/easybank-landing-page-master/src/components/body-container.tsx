480d6083e49ae4d08a8bdb35fe136351f2064ae9
Greg Schultz <gjs86@cornell.edu>
2020-03-31 22:16:34 -0400

    HOC getInitialProps trigger working

--- challenges/easybank-landing-page-master/src/components/body-container.tsx
+++ challenges/easybank-landing-page-master/src/components/body-container.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-const BodyContainer = (Component: any) => props => {
-  return (
-    <section id={props.id} className="body-container">
-      <Component {...props} />
-    </section>
-  );
+const BodyContainer = (Component: any) => {
+  const FuncComponent = ({ id }) => {
+    return (
+      <section id={id} className="body-container">
+        <Component />
+      </section>
+    );
+  };
+
+  FuncComponent.getInitialProps = async ctx => {
+    // If Page/Component has a `getInitialProps`, we should call it.
+    const props =
+      Component.getInitialProps && (await Component.getInitialProps(ctx));
+    return { ...props };
+  };
+
+  return FuncComponent;
 };
 
 export default BodyContainer;
