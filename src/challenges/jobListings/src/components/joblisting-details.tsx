491f49ac99811e360b217b258d78c792cd572407
Greg Schultz <gjs86@cornell.edu>
2020-04-05 14:34:23 -0400

    finished details

---
+++ src/challenges/jobListings/src/components/joblisting-details.tsx
@@ -0,0 +1,18 @@
+const JobDetails = ({ postedAt, contract, location }) => {
+  return (
+    <div className="flex content-center">
+      <p className="pr-4">{postedAt}</p>
+      <p className="pr-4">•</p>
+      <p className="pr-4">{contract}</p>
+      <p className="pr-4">•</p>
+      <p className="pr-4">{location}</p>
+      <style jsx>{`
+        p {
+          font-size: 12px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default JobDetails;
--- src/challenges/jobListings/src/components/joblisting-information.tsx
+++ src/challenges/jobListings/src/components/joblisting-information.tsx
@@ -1,18 +1,32 @@
+import JobDetails from "./joblisting-details";
+import Pill from "./joblisting-pill";
+
 const JobListingInformation = (props) => {
   return (
     <div className="flex flex-wrap">
-      <div className="w-full bg-gray-500">
+      <div className="w-full py-1 bg-gray-500">
         <div className="flex">
-          <h1 className="company mr-2">{props.company}</h1>
+          <h2 className="company mr-2">{props.company}</h2>
           {props.new && <PillSpacer text="NEW!"></PillSpacer>}
           {props.featured && <PillSpacer text="FEATURED"></PillSpacer>}
         </div>
       </div>
-      <div className="w-full bg-gray-400">2</div>
-      <div className="w-full bg-gray-500">3</div>
+      <div className="w-full py-1 bg-gray-400">
+        <h1 className="position">{props.position}</h1>
+      </div>
+      <div className="w-full py-1 bg-gray-500">
+        <JobDetails
+          postedAt={props.postedAt}
+          contract={props.contract}
+          location={props.location}
+        />
+      </div>
       <style jsx>{`
         .company {
           color: var(--Desaturated-Dark-Cyan);
+        }
+        .position {
+          font-weight: bold;
         }
       `}</style>
     </div>
@@ -23,31 +37,6 @@ const PillSpacer = (props) => {
   return (
     <div className="px-1">
       <Pill {...props} />
-    </div>
-  );
-};
-
-const Pill = ({ text }) => {
-  const pillTypeCss = text === "NEW!" ? "pill-new" : "pill-featured";
-  const pillClass = `rounded-full  ${pillTypeCss}`;
-  return (
-    <div className={pillClass}>
-      <p className="px-2 inline-block align-middle">{text}</p>
-      <style jsx>{`
-        p {
-          color: white;
-          font-weight: bold;
-          font-size: 8px;
-          text-align: center;
-        }
-
-        .pill-new {
-          background-color: var(--Desaturated-Dark-Cyan);
-        }
-        .pill-featured {
-          background-color: black;
-        }
-      `}</style>
     </div>
   );
 };
