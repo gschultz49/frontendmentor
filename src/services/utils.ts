78b9152a2d44052bca86e6a58c598bf9e60eb37e
Greg Schultz <gjs86@cornell.edu>
2020-04-14 10:10:30 -0400

    proper format

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -14,9 +14,11 @@ export const setImagesDirectoryForProject = (imagesDir: string) => {
 
 export const setApiBaseForProject = (apiDir: string) => {
   return function getApiEndpointGivenDir(endpoint: string) {
-    console.log(`${process.env.NOW_URL}${apiDir}${endpoint}`);
-    console.log(new URL(process.env.NOW_URL, `${apiDir}${endpoint}`));
-    return new URL(process.env.NOW_URL, `${apiDir}${endpoint}`);
+    console.log(
+      new URL(`${apiDir}${endpoint}`, `https://${process.env.NOW_URL}`).href,
+    );
+    return new URL(`${apiDir}${endpoint}`, `https://${process.env.NOW_URL}`)
+      .href;
   };
 };
 
