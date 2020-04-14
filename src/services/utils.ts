0d697ada6572b03e11fa6cd9402b8853ad870984
Greg Schultz <gjs86@cornell.edu>
2020-04-14 09:55:51 -0400

    new url test

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -15,7 +15,8 @@ export const setImagesDirectoryForProject = (imagesDir: string) => {
 export const setApiBaseForProject = (apiDir: string) => {
   return function getApiEndpointGivenDir(endpoint: string) {
     console.log(`${process.env.NOW_URL}${apiDir}${endpoint}`);
-    return `${process.env.NOW_URL}${apiDir}${endpoint}`;
+    console.log(new URL(process.env.NOW_URL, `${apiDir}${endpoint}`));
+    return new URL(process.env.NOW_URL, `${apiDir}${endpoint}`);
   };
 };
 
